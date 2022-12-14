1ea31f64a8bbdcb7efb84351ec1d1e5a75116881
sbiljeek <sbiljeekdev@gmail.com>
2022-12-14 14:18:10 +0300

    Initial Commit

---
+++ InstagramPinchToZoomImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramPinchToZoomImage
+//
+//  Created by Salman Biljeek on 12/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
